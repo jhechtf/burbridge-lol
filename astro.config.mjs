fa57c590f101b661ca270c036658549ee4fef2cf
Jim Burbridge <jhechtf@gmail.com>
2024-12-20 22:50:16 -0800

    fix(stories): Fixes config to update to Astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  env: {
+    schema: {
+      NODE_ENV: envField.string({ context: 'server', access: 'secret' }),
+    }
+  }
 });
