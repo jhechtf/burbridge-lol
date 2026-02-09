21e4b4ca56d2a3bcda3eb40c19ad055cf79e6dd7
Jim Burbridge <jhechtf@gmail.com>
2026-02-08 23:59:23 -0800

    fix: Add empty state (#34)

---
+++ web/src/lib/util.ts
@@ -0,0 +1,37 @@
+export type Contents =
+  | {
+      type: 'text';
+      text: string;
+      code?: boolean;
+      bold?: boolean;
+      italic?: boolean;
+    }
+  | {
+      type: 'paragraph';
+      children: Contents[];
+    }
+  | {
+      type: 'heading';
+      children: Contents[];
+      level: number;
+    };
+
+export function calculateReadingTime(contents: Contents[]): number {
+  /**
+   * 1. Iterate over the top level contents
+   * 2. If type === paragraph, split the text attribute to get the number of words.
+   * 3. If anything else, add the children element to the queue.
+   */
+  const queue = contents.slice();
+  let words = 0;
+  let current: Contents = queue.shift();
+
+  while (current) {
+    if (current.type === 'text')
+      words += current.text.trim().split(/\s+/).length;
+    else queue.unshift(...current.children);
+    current = queue.shift();
+  }
+
+  return Math.round(words / 255);
+}
